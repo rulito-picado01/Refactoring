c451b377e9f370d9b0423dc3e18735eba33b5321
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2025-04-13 22:15:55 -0300

    renaming variable

--- src/main/java/oop2/tp3/ejercicio5/Calculador.java
+++ src/main/java/oop2/tp3/ejercicio5/Calculador.java
@@ -2,17 +2,15 @@ package oop2.tp3.ejercicio5;
 
 import java.util.List;
 
-
 public class Calculador {
 
     public String reporte(Factura factura, List<Evento> eventos) {
         float totalAmount = 0;
-        float volumeCredits = 0;
+        float creditos = 0;
         var result = "Facturación para " + factura.nombreCliente() + System.lineSeparator();
         var actuaciones = factura.actuaciones();
         for (var actuacion : actuaciones) {
             float monto = 0;
-
             var tipo = eventos.stream()
                     .filter(e -> e.nombreEvento().equals(actuacion.nombreEvento()))
                     .findFirst()
@@ -38,16 +36,16 @@ public class Calculador {
             }
 
             // creditos a ganar
-            volumeCredits += Math.max(actuacion.numberoEspectadores() - 30, 0);
+            creditos += Math.max(actuacion.numberoEspectadores() - 30, 0);
             // creditos extras para comedia
             if ("Comedia".equals(tipo)) {
-                volumeCredits += Math.floor(actuacion.numberoEspectadores() / 5);
+                creditos += Math.floor(actuacion.numberoEspectadores() / 5);
             }
             result += actuacion.nombreEvento() + ": " + monto + ". Asientos: " + actuacion.numberoEspectadores() + System.lineSeparator();//` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
             totalAmount += monto;
         }
         result += "Monto ganado: " + totalAmount + System.lineSeparator();
-        result += "Créditos ganados: " + volumeCredits + System.lineSeparator();
+        result += "Créditos ganados: " + creditos + System.lineSeparator();
 
         return result;
     }
